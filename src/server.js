1098a7b4553ea02256f29b37317bd5ae18239cc4
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-05-25 14:30:18 -0300

    enviando o metodo post

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
    
 app.get("/message/:id", (request, response) =>{
     const { id } = request.params; 
     response.send(`id:${id}`)
 })
-
+app.post("/user", (request, response)=> {
+    const {name, email, password} = request.body;
+    response.send(`Usuario: ${name}. E-mail: ${email}. E a senha é ${password}`)
+}) 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server está na porta ${PORT}`));
